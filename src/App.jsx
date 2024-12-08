4e85ec7646ef8161c006209edc1566a2bc255a95
manojachanta72 <manojachanta7172@gmail.com>
2024-12-08 12:23:56 +0530

    overflow-scrollbar-removed

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import Footer from './components/Footer'
 
 const App = () => {
   return (
-    <div className="selection:text-cyan-400 w-full text-white [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)] bg-cover">
+    <div id='main' className="overflow-y-hidden selection:text-cyan-400 w-full text-white [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)] bg-cover">
       <Navbar/>
       <Hero/>
       <Projects/>
