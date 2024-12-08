9dbbd749c04b8e93d336e4d83a01656491d85153
manojachanta72 <manojachanta7172@gmail.com>
2024-12-08 12:08:12 +0530

    modified-skills-section

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -7,17 +7,17 @@ const Skills = () => {
         <h1 className='text-center text-4xl font-semibold'>Skills</h1>
       </div>
       <div className='grid select-none selection:none w-full gap-14 md:px-24 grid-cols-3 md:grid-cols-5 pb-16'>
-        <div tooltip="java" className='size-20 rounded-[50%]'><img src="/skills/java.png"   className="animate-rotate-x " /></div>
-        <div className='size-20 rounded-[50%]'><img src="/skills/python.png" className="animate-rotate-x selection:none " /></div>
-        <div className='size-20 rounded-[50%]'><img src="/skills/html.png"   className="animate-rotate-x " /></div>
-        <div className='size-20 rounded-[50%]'><img src="/skills/css.png"    className="animate-rotate-x " /></div>
-        <div className='size-16 rounded-[50%] place-content-center'><img src="/skills/tailwind-css.svg" className="animate-rotate-x " /></div>
-        <div className='size-20 rounded-[50%]'><img src="/skills/js.png"     className="animate-rotate-x " /></div>
-        <div className='size-20 rounded-[50%]'><img src="/skills/react.png" className="animate-rotate-x " /></div>
-        <div className='size-20 rounded-[50%]'><img src="/skills/nodejs.png" className="animate-rotate-x " /></div>
-        <div className='size-20 rounded-[50%]'><img src="/skills/mongo.png" className="animate-rotate-x " /></div>
-        <div className='size-20 rounded-[50%]'><img src="/skills/git.png" className="animate-rotate-x " /></div>
-        <div className='size-20 rounded-[50%] '><img src="/skills/github.png" className="animate-rotate-x bg-white rounded-[50%]" /></div>
+        <div tooltip="java" className='size-20 rounded-[50%]'><img src="/skills/java.png"   className="" /></div>
+        <div className='size-20 rounded-[50%]'><img src="/skills/python.png" className="selection:none " /></div>
+        <div className='size-20 rounded-[50%]'><img src="/skills/html.png"   className="" /></div>
+        <div className='size-20 rounded-[50%]'><img src="/skills/css.png"    className="" /></div>
+        <div className='size-16 rounded-[50%] place-content-center'><img src="/skills/tailwind-css.svg" className="" /></div>
+        <div className='size-20 rounded-[50%]'><img src="/skills/js.png"     className="" /></div>
+        <div className='size-20 rounded-[50%]'><img src="/skills/react.png" className="" /></div>
+        <div className='size-20 rounded-[50%]'><img src="/skills/nodejs.png" className="" /></div>
+        <div className='size-20 rounded-[50%]'><img src="/skills/mongo.png" className="" /></div>
+        <div className='size-20 rounded-[50%]'><img src="/skills/git.png" className="" /></div>
+        <div className='size-20 rounded-[50%] '><img src="/skills/github.png" className="bg-white rounded-[50%]" /></div>
       </div>
     </div>
   )
