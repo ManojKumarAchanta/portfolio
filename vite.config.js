d6f5d8bff55eda6c210e0cb237ea81a0060c5f62
ManojKumarAchanta <manojachanta7172@gmail.com>
2025-05-24 21:13:48 +0530

    added config

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  allowedHosts: ["portfolio-bq4m.onrender.com"],
+  server: {
+    allowedHosts: ["portfolio-bq4m.onrender.com"],
+  },
 });
